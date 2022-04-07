0a6aefba712a1a472fe629d87455920d6439c505
VictoriaRyd <victoriarydina@gmail.com>
2022-04-07 20:10:00 +0300

    When the Button is clicked, launch a camera app to take a picture

--- app/src/main/java/com/example/homework_implicitintent/MainActivity.kt
+++ app/src/main/java/com/example/homework_implicitintent/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.example.homework_implicitintent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mWebsiteEditText: EditText
+    private lateinit var mLocationEditText: EditText
+    private lateinit var mShareTextEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mWebsiteEditText = findViewById(R.id.website_edittext)
+        mLocationEditText = findViewById(R.id.location_edittext)
+        mShareTextEditText = findViewById(R.id.share_edittext)
+    }
+
+    fun openWebsite(view: View) {
+        val url = mWebsiteEditText.text.toString()
+        val webpage: Uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent)
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!")
+        }
+    }
+
+    fun openLocation(view: View?) {
+        val loc: String = mLocationEditText.text.toString()
+        val addressUri = Uri.parse("geo:0,0?q=$loc")
+        val intent = Intent(Intent.ACTION_VIEW, addressUri)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!")
+        }
+    }
+
+    fun shareText(view: View) {
+        val txt = mShareTextEditText.text.toString()
+        val mimeType = "text/plain"
+        ShareCompat.IntentBuilder(this)
+            .setType(mimeType)
+            .setChooserTitle(getString(R.string.setChooserTitle))
+            .setText(txt)
+            .startChooser()
+    }
+
+    fun takePicture(view: View) {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(takePictureIntent)
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!")
+        }
     }
 }
